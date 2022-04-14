f411ffafb083c77e96da84fabc55ff5fb6903b5f
Dmitry <koliasam1@gmail.com>
2022-04-14 20:33:12 +0300

    6 дз.правки

--- src/main/java/vtb/geekbrains/test6/Threads.java
+++ src/main/java/vtb/geekbrains/test6/Threads.java
@@ -25,53 +25,51 @@ public class Threads {
         float[] arr1 = new float[HALF];
         float[] arr2 = new float[HALF];
         a = System.currentTimeMillis();
+        Arrays.fill(arr2,0,HALF,1);
+        Arrays.fill(arr1,0,HALF,1);
 
-        new Thread(){
-            @Override
-            public void run() {
-                Arrays.fill(arr2,0,HALF,1);
-                System.arraycopy(arr2,0,arr,0,HALF);
-                System.out.println(System.currentTimeMillis() - a);
-            }
-        }.start();
+        System.arraycopy(arr2,0,arr,0,HALF);
+        System.arraycopy(arr1,0,arr,HALF,HALF);
 
-        new Thread(){
-            @Override
-            public void run() {
-                Arrays.fill(arr1,0,HALF,1);
-                System.arraycopy(arr1,0,arr,HALF,HALF);
-                System.out.println(System.currentTimeMillis() - a);
-            }
-        }.start();
-
-        new Thread(){
+        Thread one = new Thread(){
             @Override
             public void run() {
 
                 System.arraycopy(arr,HALF,arr2,0,HALF);
-                for (int i = 0; i < SIZE; i++) {
-                    arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
-                            Math.cos(0.4f + i / 2));
+                for (int i = 0, j = HALF; i < SIZE; i++,j++) {
+                    arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + j) / 5) * Math.cos(0.2f + (i + j) / 5) *
+                            Math.cos(0.4f + (i + j) / 2));
                 }
-                System.arraycopy(arr2,0,arr,arr2.length,HALF);
-                System.out.println(System.currentTimeMillis() - a);
             }
-        }.start();
+        };
 
-        new Thread(){
+        Thread two = new Thread(){
             @Override
             public void run() {
                 System.arraycopy(arr,0,arr1,0,HALF);
-                for (int i = 0; i < SIZE; i++) {
-                    arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) *
-                            Math.cos(0.4f + i / 2));
+                for (int i = 0,j = HALF; i < SIZE; i++,j++) {
+                    arr[i] = (float)(arr[i] * Math.sin(0.2f + (i + j)/ 5) * Math.cos(0.2f + (i + j) / 5) *
+                            Math.cos(0.4f + (i + j) / 2));
                 }
-                System.arraycopy(arr1,0,arr,arr1.length,HALF);
-                System.out.println(System.currentTimeMillis() - a);
+
             }
-        }.start();
+        };
+
+        one.start();
+        two.start();
 
 
+        try {
+            one.join();
+            two.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.arraycopy(arr2,0,arr,arr2.length,HALF);
+        System.out.println(System.currentTimeMillis() - a);
+
+        System.arraycopy(arr1,0,arr,arr1.length,HALF);
+        System.out.println(System.currentTimeMillis() - a);
     }
 }
 
